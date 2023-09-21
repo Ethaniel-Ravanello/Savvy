386c634d352d31f1a9aeb78b6eba93c0b9783e6d
iqbalnew <iqbalmahadi14@gmail.com>
2023-09-21 11:22:15 +0700

    feat: User Controller

--- backend/Controller/UsersController.js
+++ backend/Controller/UsersController.js
@@ -1,7 +1,31 @@
 import Users from "../Models/Users.js";
 
-// export const getUsers = async(req, res) => {
-//     try{
-//         const response = await Users.
-//     }
-// }
+export const getUserById = async(req, res) => {
+    const {userId} = req.body
+    try{
+        const response = await Users.findById(userId)
+        if(!response){
+            return res.status(400).json({message: "User Not Found"})
+        }
+        return res.status(200).json(response)
+    }
+    catch{
+        return res.status(500).json({message: "Internal Server Error"})
+    }
+}
+
+export const updateUserById = async(req, res) => {
+    const {userId} = req.body
+    try{
+        const response = await Users.findByIdAndUpdate(userId, req.body, {
+            new: true
+        })
+        if(!response){
+            res.status(400).json({message: "User Not Found"})
+        }
+        return res.status(200).json(response)
+    }
+    catch{
+        return res.status(500).json({message : "Internal Server Error"})
+    }
+}
