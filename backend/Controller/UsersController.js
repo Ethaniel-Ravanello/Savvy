367765a3ecb89d1ccef0f7a222ca33bae219c107
Ethanie Ravanello <ravaneloe@gmail.com>
2023-09-21 11:33:16 +0700

    feat: User Controller

--- backend/Controller/UsersController.js
+++ backend/Controller/UsersController.js
@@ -21,11 +21,27 @@ export const updateUserById = async(req, res) => {
             new: true
         })
         if(!response){
-            res.status(400).json({message: "Users Not Found"})
+           return res.status(400).json({message: "User Not Found"})
         }
         return res.status(200).json(response)
     }
     catch{
         return res.status(500).json({message : "Internal Server Error"})
+    }
+}
+
+export const deleteUserById = async(req, res) => {
+    const {userId} = req.body
+    try{
+        const response = await Users.findByIdAndDelete(userId)
+
+        if(!response){
+            return res.status(400).json({message: "User Not Found"})
+        }
+
+        return res.status(200).json(response)
+    }
+    catch{
+        return res.status(500).json({message: "Internal Server Error"})
     }
 }
