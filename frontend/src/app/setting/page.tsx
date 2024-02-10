564d9bc3ec16ebc8bb66c0bfe252649db4f85467
Ethaniel <ravaneloe@gmail.com>
2024-02-10 22:39:22 +0700

    feat: New Try 2

--- frontend/src/app/setting/page.tsx
+++ frontend/src/app/setting/page.tsx
@@ -193,7 +193,7 @@ const Page = () => {
               id="Hehe-Password"
               onChange={handleChange}
               autoComplete="new-password"
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-transparent dark:border-gray-600 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              className="bg-gray-50 border Please border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-transparent dark:border-gray-600 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             />
           </div>
 
